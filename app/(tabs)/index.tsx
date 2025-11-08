2ad83644a09f84948ae085a654c36b7fe4c01819
Kaito Stenroos <kaito.stenroos@outlook.com>
2025-11-08 18:18:55 +0200

    First commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,107 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import * as SQLite from "expo-sqlite";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet } from "react-native";
+import { Appbar, Card, PaperProvider, Text } from "react-native-paper";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+export default function ListSelector() {
+  const db = SQLite.openDatabaseSync("listdb");
+  const [rankedLists, setRankedLists] = useState([])
 
-export default function HomeScreen() {
+  const dropTable = async () => {
+    await db.execAsync(`
+      DROP TABLE RankedLists`)
+  }
+
+  const initialize = async () => {
+    try {
+      await db.execAsync(`
+        CREATE TABLE IF NOT EXISTS RankedLists (id INTEGER PRIMARY KEY, title TEXT NOT NULL, bannersrc TEXT, description TEXT, created TEXT NOT NULL, updated TEXT NOT NULL );
+      `);
+    } catch (error) {
+      console.error("Could not open database for lists", error);
+    }
+  };
+
+  const insertExampleData = async () => {
+    try {
+      const tableCheck = await db.getAllAsync('SELECT * FROM RankedLists')
+      const tableIsEmpty = tableCheck.length === 0;
+      console.log(tableCheck)
+
+      if (tableIsEmpty) {
+        await db.execAsync(`
+          INSERT INTO RankedLists (id, title, bannersrc, description, created, updated) VALUES
+            (1, 'My Top Games', 'https://picsum.photos/700', 'A short ranked list of favorite games.', '2025-11-08', '2025-11-09'),
+            (2, 'Top Movies', 'https://picsum.photos/700', 'All-time favorite movies.', '2025-11-07', '2025-11-08');
+        `);
+      } else {
+        console.log("Interting example data failed: Table has data")
+      }
+    } catch (error) {
+      console.error("Could not insert example data", error )
+    }
+  }
+
+  const updateList = async () => {
+    try {
+      const list = await db.getAllAsync("SELECT * from RankedLists");
+      setRankedLists(list);
+      console.log("UPDATED: ", list)
+    } catch (error) {
+      console.error("Could not get items", error);
+    }
+  };
+
+  useEffect(() => {
+    const bootstrap = async () => {
+      await dropTable(); //for dev purposes
+      await initialize();
+      await insertExampleData();
+      await updateList();
+    };
+
+    bootstrap();
+  }, []);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <PaperProvider>
+      <Appbar.Header>
+        <Appbar.Content title="Ranked List" />
+      </Appbar.Header>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Card style={styles.card}>
+            <Card.Cover source={{ uri: item.bannersrc }} style={styles.cardCover} />
+            <Card.Content>
+              <Text variant="headlineLarge" style={styles.title}>{item.title}</Text>
+              <Text variant="bodyLarge" style={styles.description}>{item.description}</Text>
+              <Text variant="labelSmall" style={styles.date}>Created: {item.created}</Text>
+              <Text variant="labelSmall" style={styles.date}>Updated: {item.updated}</Text>
+            </Card.Content>
+          </Card>
+        )}
+        data={rankedLists}
+      />
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  card: {
+    margin: 10,
   },
-  stepContainer: {
-    gap: 8,
+  cardCover: {
+    marginBottom: 8,
+
+  },
+  title: {
     marginBottom: 8,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  description: {
+    marginBottom: 8,
+  },
+  date: {
+    marginBottom: 4,
   },
 });
